9c8764e829b0087581c1889f3fcec3a34646d170
Ben-Hur <benhur467@gmail.com>
2022-08-31 10:51:28 -0300

    aula4

--- pages/estadios.vue
+++ pages/estadios.vue
@@ -1,99 +1,96 @@
 <template>
   <section>
-  <!-- TITULO -->
-  <h1 class="title is-1 mb-5 mt-5">Estadios</h1>
+    <!-- TITULO -->
+    <h1 class="title is-1 mb-5 mt-5">Estadios</h1>
 
-  <!-- COLUNAS -->
-  <div class="columns">
-    <!-- COLUNAS 1-->
-    <div class="column is-3"> 
-      <!-- ESTADIO 1 -->
-      <div class="estadio mb-5"> 
-        <figure class="image is-150x150">
-          <img src="https://bulma.io/images/placeholders/128x128.png">
-        </figure>
-        <div class="buttons">
-          <b-button type="is-warning" expanded> Al bayt Satadium </b-button>
+    <!-- COLUNAS -->
+    <div class="columns">
+      <!-- COLUNAS 1-->
+      <div class="column is-3">
+        <!-- ESTADIO 1 -->
+        <div class="estadio mb-5">
+          <figure class="image is-150x150">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
+          </figure>
+          <div class="buttons">
+            <b-button type="is-warning" expanded> Al bayt Satadium </b-button>
+          </div>
+        </div>
+
+        <!-- ESTADIO 2 -->
+        <div class="estadio mb-5">
+          <figure class="image is-150x150">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
+          </figure>
+          <div class="buttons">
+            <b-button type="is-warning" expanded>Qatar</b-button>
+          </div>
         </div>
       </div>
 
-      <!-- ESTADIO 2 -->
-      <div class="estadio mb-5" > 
-        <figure class="image is-150x150">  <!-- ESTADIO 1 -->
-          <img src="https://bulma.io/images/placeholders/128x128.png">
+      <!-- COLUNAS 2-->
+      <div class="column is-3">
+        <div class="estadio mb-5">
+          <figure class="image is-150x150">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
+          </figure>
+          <div class="buttons">
+            <b-button type="is-warning" expanded
+              >Khalifa International</b-button
+            >
+          </div>
+        </div>
+
+        <div class="estadio mb-5">
+          <figure class="image is-150x150">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
+          </figure>
+          <div class="buttons">
+            <b-button type="is-warning" expanded>Stadium 974</b-button>
+          </div>
+        </div>
+      </div>
+
+      <!-- COLUNAS 3-->
+
+      <div class="column is-3">
+        <figure class="image is-150x150">
+          <img src="https://bulma.io/images/placeholders/128x128.png" />
+        </figure>
+        <div class="buttons">
+          <b-button type="is-warning" expanded>Qatar</b-button>
+        </div>
+
+        <figure class="image is-150x150">
+          <img src="https://bulma.io/images/placeholders/128x128.png" />
         </figure>
         <div class="buttons">
           <b-button type="is-warning" expanded>Qatar</b-button>
         </div>
       </div>
+
+      <!-- COLUNAS 4-->
+
+      <div class="column is-3">
+        <div class="estadio mb-5">
+          <figure class="image is-150x150">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
+          </figure>
+          <div class="buttons">
+            <b-button type="is-warning" expanded>Al Thumama</b-button>
+          </div>
+        </div>
+
+        <div class="estadio mb-5">
+          <figure class="image is-150x150">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
+          </figure>
+          <div class="buttons">
+            <b-button type="is-warning" expanded>Al Janoub</b-button>
+          </div>
+        </div>
+      </div>
     </div>
-
-    <!-- COLUNAS 2-->
-    <div class="column is-3">
-      <div class="estadio mb-5">
-        <figure class="image is-150x150">
-          <img src="https://bulma.io/images/placeholders/128x128.png">
-        </figure>
-        <div class="buttons">
-          <b-button type="is-warning" expanded>Khalifa International</b-button>
-        </div>
-      </div>
-
-      <div class="estadio mb-5">
-        <figure class="image is-150x150">
-          <img src="https://bulma.io/images/placeholders/128x128.png">
-        </figure>
-        <div class="buttons">
-          <b-button type="is-warning" expanded>Stadium 974</b-button>
-        </div>
-      </div>
-           
-    </div>
-
-    <!-- COLUNAS 3-->
-    
-    <div class="column is-3">
-      <figure class="image is-150x150">
-        <img src="https://bulma.io/images/placeholders/128x128.png">
-      </figure>
-      <div class="buttons">
-        <b-button type="is-warning" expanded>Qatar</b-button>
-      </div>
-
-      <figure class="image is-150x150">
-        <img src="https://bulma.io/images/placeholders/128x128.png">
-      </figure>
-      <div class="buttons">
-        <b-button type="is-warning" expanded>Qatar</b-button>
-      </div>
-      
-    </div>
-
-     <!-- COLUNAS 4-->
-    
-    <div class="column is-3">
-      <div class="estadio mb-5">
-        <figure class="image is-150x150">
-          <img src="https://bulma.io/images/placeholders/128x128.png">
-        </figure>
-        <div class="buttons">
-          <b-button type="is-warning" expanded>Al Thumama</b-button>
-        </div>
-      </div>
-
-      <div class="estadio mb-5">
-        <figure class="image is-150x150">
-          <img src="https://bulma.io/images/placeholders/128x128.png">
-        </figure>
-        <div class="buttons">
-          <b-button type="is-warning" expanded>Al Janoub</b-button>
-        </div>
-      </div>
-           
-    </div>   
-  </div>
-
-
   </section>
 </template>
 
--- pages/selecoes.vue
+++ pages/selecoes.vue
@@ -1,16 +1,108 @@
 <template>
-    <section class="section">
-        <figure class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
-        </figure>
+  <section class="section">
+    <div class="columns">
+      <!-- COLUNA 1-->
+      <div class="column">
+        <div class="columns">
+          <!-- COLUNA 1-->
+          <div class="column">
+            <figure class="image is-150x150">
+              <img
+                src="https://static.todamateria.com.br/upload/ba/nd/bandeiradobrasil-2-cke.jpg"
+              />
+            </figure>
+            <h4 class="subtitle is-4 has-text-centered">Brasil</h4>
+          </div>
+          <!-- COLUNA 2-->
+          <div class="column">
+            <figure class="image is-150x150">
+              <img
+                src="https://cdn.folhape.com.br/img/pc/1100/1/dn_arquivo/2022/08/whatsapp-image-2022-08-04-at-134450.jpeg"
+              />
+            </figure>
+          </div>
+        </div>
+      </div>
 
-       
+      <!-- COLUNAS 2 -->
+      <div class="column">
+        <div class="columns">
+          <div class="column">
+            <div class="atleta mb-5">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
 
+            <div class="atleta mb-5">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
 
+            <div class="atleta mb-5">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
 
+            
+          </div>
+          <div class="column">
+            <div class="atleta">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="atleta">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="atleta">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-
-
-
-    </section>  
+      
+    </div>
+  </section>
 </template>
