f75422750ba9479a164fc90a46cae9e426262c09
Ben-Hur <benhur467@gmail.com>
2022-08-31 08:51:01 -0300

    selecoes

---
+++ pages/selecoes.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="section">
+        <figure class="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+        </figure>
+
+
+
+
+
+
+
+    </section>  
+</template>
