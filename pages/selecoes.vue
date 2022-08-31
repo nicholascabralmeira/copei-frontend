f12f25cb13b145e74c18838e2c22b57234e617ac
Ben-Hur <benhur467@gmail.com>
2022-08-31 12:38:37 -0300

    sel

--- pages/selecoes.vue
+++ pages/selecoes.vue
@@ -3,23 +3,120 @@
     <div class="columns">
       <!-- COLUNA 1-->
       <div class="column">
+        <!-- SEPARACAO EM 2 -->
         <div class="columns">
-          <!-- COLUNA 1-->
+          <!--COLUNA BANDEIRA -->
           <div class="column">
             <figure class="image is-150x150">
               <img
                 src="https://static.todamateria.com.br/upload/ba/nd/bandeiradobrasil-2-cke.jpg"
               />
             </figure>
-            <h4 class="subtitle is-4 has-text-centered">Brasil</h4>
           </div>
-          <!-- COLUNA 2-->
+
+          <!-- COLUNA TIME-->
           <div class="column">
             <figure class="image is-150x150">
               <img
                 src="https://cdn.folhape.com.br/img/pc/1100/1/dn_arquivo/2022/08/whatsapp-image-2022-08-04-at-134450.jpeg"
               />
             </figure>
+          </div>
+        </div>
+
+        <!-- SEPARACAO EM 4 -->
+        <div class="columns">
+          <!-- PRIMEIRA COLUNA -->
+          <div class="column">
+            <!-- Atleta 1 -->
+            <div class="atleta mb-6">
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded> Casemiro </b-button>
+              </div>
+            </div>
+
+            <!-- Atleta 2 -->
+            <div class="atleta mb-5">
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded> Casemiro </b-button>
+              </div>
+            </div>
+          </div>
+
+          <!-- SEGUNDA COLUNA -->
+          <div class="column">
+            <!-- Atleta 1 -->
+            <div class="atleta mb-6">
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded> Neymar </b-button>
+              </div>
+            </div>
+            <!-- Atleta 2 -->
+            <div class="atleta">
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded> Marquinhos </b-button>
+              </div>
+            </div>
+          </div>
+
+          <!-- TERCEIRA COLUNA -->
+          <div class="column">
+            <!-- Atleta 1 -->
+            <div class="atleta mb-6">
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded> Neymar </b-button>
+              </div>
+            </div>
+
+            <!-- Atleta 2 -->
+            <div class="atleta">
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded> Marquinhos </b-button>
+              </div>
+            </div>
+
+            
+            
+            
+          </div>
+          <!-- QUARTA COLUNA -->
+          <div class="column">
+            <!-- Atleta 1 -->
+            <div class="atleta mb-6">
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded> Marquinhos </b-button>
+              </div>
+            </div>
+            <!-- Atleta 2 -->
+            <div class="atleta">
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded> Marquinhos </b-button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -60,11 +157,31 @@
                 </b-button>
               </div>
             </div>
+          </div>
+          <div class="column">
+            <div class="atleta mb-5">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
 
-            
-          </div>
-          <div class="column">
-            <div class="atleta">
+            <div class="atleta mb-5">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
+
+            <div class="atleta mb-5">
               <figure class="image is-150x150">
                 <img src="https://bulma.io/images/placeholders/128x128.png" />
               </figure>
@@ -76,7 +193,29 @@
             </div>
           </div>
           <div class="column">
-            <div class="atleta">
+            <div class="atleta mb-5">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
+
+            <div class="atleta mb-5">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
+
+            <div class="atleta mb-5">
               <figure class="image is-150x150">
                 <img src="https://bulma.io/images/placeholders/128x128.png" />
               </figure>
@@ -88,7 +227,29 @@
             </div>
           </div>
           <div class="column">
-            <div class="atleta">
+            <div class="atleta mb-5">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
+
+            <div class="atleta mb-5">
+              <figure class="image is-150x150">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+              <div class="buttons">
+                <b-button type="is-warning" expanded>
+                  Al bayt Satadium
+                </b-button>
+              </div>
+            </div>
+
+            <div class="atleta mb-5">
               <figure class="image is-150x150">
                 <img src="https://bulma.io/images/placeholders/128x128.png" />
               </figure>
@@ -101,8 +262,6 @@
           </div>
         </div>
       </div>
-
-      
     </div>
   </section>
 </template>
