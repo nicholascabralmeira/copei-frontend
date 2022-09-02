60a6df77f64343628f743c1b55d3869874c6124d
jose-elias <josesoliveira2009@hotmail.com>
2022-09-02 08:06:20 -0300

    figurinhas

--- pages/fig.vue
+++ pages/fig.vue
@@ -1,6 +1,21 @@
 <template>
-  <section>
-<!-- FIGURINHA -->
+  <section class="section">
+   <div class="columns">
+      <div class="column is-3">
+        <figure class="image is-150x150">
+          <img src="https://bulma.io/images/placeholders/128x128.png" />
+        </figure>
+        <div class="box has-text-centered">
+          NOME DA FIGURINHA
+        </div>
+      </div>
+      <div class="column is-9">
+        <h2 class="title is-2 has-text-centered">NOME DA FIGURINHA</h2>
+        <p>Descrição da figurinha: NUM 00 | JOGADOR ou ESCUDO ou TIME ou ESPECIAL ou ESTADIO </p>
+        <p>Número de figurinhas disponíveis/cadastradas no site: XXX </p>
+        <p>Preço médio das figurinhas cadastradas no site: R$ XXX,XX </p>
+      </div>
+    </div>
    
 
   </section>
