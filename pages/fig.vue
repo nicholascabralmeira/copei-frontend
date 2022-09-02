f29284034c483e059f94ae9b19598e218aad31ad
jose-elias <josesoliveira2009@hotmail.com>
2022-09-02 10:46:47 -0300

    Update Pagina Figurinhas

--- pages/fig.vue
+++ pages/fig.vue
@@ -1,5 +1,8 @@
 <template>
   <section class="section">
+
+    <!-- HEADER DA FIGURINHA -->
+
    <div class="columns">
       <div class="column is-3">
         <figure class="image is-150x150">
@@ -10,10 +13,343 @@
         </div>
       </div>
       <div class="column is-9">
-        <h2 class="title is-2 has-text-centered">NOME DA FIGURINHA</h2>
-        <p>Descrição da figurinha: NUM 00 | JOGADOR ou ESCUDO ou TIME ou ESPECIAL ou ESTADIO </p>
-        <p>Número de figurinhas disponíveis/cadastradas no site: XXX </p>
-        <p>Preço médio das figurinhas cadastradas no site: R$ XXX,XX </p>
+        <h2 class="title is-2 has-text-centered">NOME DA FIGURINHA | NUM 00</h2>
+        <p><strong>Descrição da figurinha:</strong></p>
+        <br>
+        <p>NUM 00 | JOGADOR ou ESCUDO ou TIME ou ESPECIAL ou ESTADIO</p>
+        <br>
+        <p> <strong>Número de figurinhas disponíveis/cadastradas no site:</strong> </p>
+        <br>
+        <p>XXX</p>
+        <br>
+        <p> <strong>Preço médio das figurinhas cadastradas no site:</strong> </p>
+        <br>
+        <p> R$ XXX,XX </p>
+      </div>
+    </div>
+
+    <!-- USUARIOS E FIGURINHAS DISPONÍVEIS-->
+
+    <div class="container">
+      <div class="notification is-primary">
+        <div class="columns">
+          <div class="column is-3">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>    
+          </div>
+          <div class="column is-3">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>    
+          </div>
+          <div class="column is-3">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>    
+          </div>
+          <div class="column is-3">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Ben-Hur</p>
+                    <p class="subtitle is-6">@benhurzinho</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                 <p>Quantidade de figurinhas: XX</p>
+                 <p>Estado da figurinha: nova/amassada</p>
+                 <p>Preço: R$ XX,XX</p>
+                 <p>Aceita trocas: Sim/Não</p>
+                 <div class="buttons">
+                   <b-button type="is-primary" expanded>Entrar em contato</b-button>
+                 </div>
+                </div>
+              </div>
+            </div>    
+          </div>
+        </div>
       </div>
     </div>
    
